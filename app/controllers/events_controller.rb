315ef7c9375ea08c952c0f02e6fdae5706cebc03
Prateek Bhatnagar <bprateek10@yahoo.in>
2024-01-11 16:58:31 +0530

    cleaning code in controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,19 +6,20 @@ class EventsController < ApplicationController
 
   def create
     event_type = params[:event_type]
+    
     return redirect_to events_path, alert: 'Invalid event type' unless valid_event_type?(event_type)
+
     response = IterableService.create_event(event_type, current_user.id)
+
     if response[:success]
       case event_type
       when 'EventB'
         IterableService.send_email_notification(current_user.email) 
       end
 
-      flash[:notice] = "Event #{event_type} created successfully"
-      redirect_to events_path
+      redirect_to events_path, notice: "Event #{event_type} created successfully"
     else
-      flash[:alert] = "Unable to create event with following error: #{response[:error]}"
-      redirect_to events_path, alert: 
+      redirect_to events_path, alert: "Unable to create event with following error: #{response[:error]}"
     end
   end
 
